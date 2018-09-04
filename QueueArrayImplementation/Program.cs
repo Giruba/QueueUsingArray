eda2d477d09758e36b09c1b048a45d840712208a
gibeulah <gibeulah@microsoft.com>
2018-09-05 01:35:26 +0530

    First Commit

--- QueueArrayImplementation/Program.cs
+++ QueueArrayImplementation/Program.cs
@@ -6,7 +6,32 @@ namespace QueueArrayImplementation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Queue implementation using Array!");
+            Console.WriteLine("---------------------------------");
+
+            Queue q = new Queue(100);
+            int option = 0;
+            do { 
+            Console.WriteLine("Enter the option 1 to Enqueue or 2 to Dequeue and 3 to quit!");
+            option = int.Parse(Console.ReadLine());            
+            switch (option) {
+                case 1:
+                    Console.WriteLine("Enter the item to enqueue");
+                    int element = int.Parse(Console.ReadLine());
+                    q.Enqueue(element);
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    int dequeuedElement = q.Dequeue();
+                        if (dequeuedElement != -1)
+                        {
+                            Console.WriteLine("Dequeud element is " + dequeuedElement);
+                        }
+                    break;
+                default:
+                    break;
+            }
+            } while (option != 3);
         }
     }
 }
---
+++ QueueArrayImplementation/Queue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueArrayImplementation
+{
+    class Queue
+    {
+        private int[] array;
+        private int front;
+        private int rear;
+        private int capacity;
+        private int size;
+
+        private Queue() { }
+
+        public Queue(int size) {
+            capacity = size;
+            array = new int[size];
+            front = 0;
+            rear = 0;
+        }
+
+        private void displayQueue(int optional=0) {
+            if (size == 0)
+            {
+                Console.WriteLine("Queue Contents:0");
+            }
+            else
+            {
+                Console.WriteLine("Queue Contents");
+                Console.WriteLine("--------------");
+
+                for (int i = optional; i < rear; i++)
+                {
+                    if (i == rear - 1)
+                    {
+                        Console.Write(array[i]);
+                    }
+                    else
+                    {
+                        Console.Write(array[i] + "->");
+                    }
+                }
+            }
+
+        }
+
+        public void Enqueue(int element) {
+
+            if (rear == capacity - 1)
+            {
+                Console.WriteLine("Queue is full!Cannot insert right now!");
+            }
+            else
+            {
+                array[rear++] = element;
+                size++;
+                displayQueue();
+            }
+        }
+
+
+        public int Dequeue() {
+            if (size == 0)
+            {
+                front = rear = 0;
+                Console.WriteLine("Queue is empty! Cannot dequeue!");
+                return -1;
+            }
+            size--;
+            displayQueue(front+1);
+            return array[front++];
+        }
+    }
+}
